798a286ad3905a4e8a802c2131dbea05d1202b7a
JeffernyWu <101135930+JeffernyWu@users.noreply.github.com>
2023-02-13 01:26:15 -0500

    [Added new mechancis and a win/lose condition] - players get destroyed by asteriods - players win by surviving the asteriods - win/lose condition

--- Assets/Scipts/PlayerYeet.cs
+++ Assets/Scipts/PlayerYeet.cs
@@ -6,6 +6,11 @@ public class PlayerYeet : MonoBehaviour
 {
     // initializing variables 
     public float speed = 2f;
+    public GameObject WinText;
+    public GameObject LoseText;
+    bool win_game = true;
+    public int win_time = 10000;
+    float time_counter = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -52,5 +57,27 @@ public class PlayerYeet : MonoBehaviour
             transform.position = mousePos; 
         }
         */
+
+        // iterate the time counter to count the frames 
+        time_counter++;
+
+        // if the player didn't get destroyed & survives all the astroids, they get the win
+        if (win_game == true)
+        {
+            // Once the time is up, active the win text 
+            if (time_counter == win_time)
+            {
+                WinText.SetActive(true);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // if player collides with anything, the turso gets destroyed & text is active stating
+         // that the player lost 
+        Destroy(gameObject);
+        LoseText.SetActive(true);
+        win_game = false;
     }
 }
